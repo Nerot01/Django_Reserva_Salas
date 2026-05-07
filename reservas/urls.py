e611a27eaf58650f4a6f5f2d50cb4aaff092614a
Nerot01 <Nerot01@users.noreply.github.com>
2026-05-07 15:03:52 -0400

    Ignorar archivo de credenciales.txt

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('registro/', views.registro_view, name='registro'),
     path('panel/', views.panel, name='panel'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
