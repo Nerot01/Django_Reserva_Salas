fe240156041c482a135d078d154ea626b2225ea5
Nerot01 <Nerot01@users.noreply.github.com>
2026-05-07 14:49:52 -0400

    Estructura inicial: Configuración de settings, rutas base y creación de app reservas

--- reservas/views.py
+++ reservas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Página de Inicio de Reservas")
+
+def login_view(request):
+    return HttpResponse("Página de Login")
+
+def registro_view(request):
+    return HttpResponse("Página de Registro")
